bacdf302b0351c54f845317460cad8e31dbadfd7
brian7346 <doctorbrian97@gmail.com>
2019-05-08 11:18:11 +0400

    Updates

---
+++ precache-manifest.3c9be42a41986bb81b3ae8c0ba515c73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d1abf55d24c8e0c6f3a5554c0c55bde",
+    "url": "/react-firebase-events-editor/static/media/header_bg.3d1abf55.png"
+  },
+  {
+    "revision": "83beeb1bd3c4a977a610",
+    "url": "/react-firebase-events-editor/static/js/runtime~main.83beeb1b.js"
+  },
+  {
+    "revision": "35fa4f2300df9e9fd010",
+    "url": "/react-firebase-events-editor/static/js/main.35fa4f23.chunk.js"
+  },
+  {
+    "revision": "a97a87a1fa7ad7dfe67d",
+    "url": "/react-firebase-events-editor/static/js/1.a97a87a1.chunk.js"
+  },
+  {
+    "revision": "35fa4f2300df9e9fd010",
+    "url": "/react-firebase-events-editor/static/css/main.a32fec5c.chunk.css"
+  },
+  {
+    "revision": "a97a87a1fa7ad7dfe67d",
+    "url": "/react-firebase-events-editor/static/css/1.6d604754.chunk.css"
+  },
+  {
+    "revision": "4c0829cb0eb1c4e19ada04a86713d449",
+    "url": "/react-firebase-events-editor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/mst-task-events/precache-manifest.6bb33ca46507b251c8fb7154f11e7e34.js"
+  "/react-firebase-events-editor/precache-manifest.3c9be42a41986bb81b3ae8c0ba515c73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/mst-task-events/index.html", {
+workbox.routing.registerNavigationRoute("/react-firebase-events-editor/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
